33907373156cf3fa8b2cd1d846ed400d4ea09494
GeorgeIliop <georgeiliopp@gmail.com>
2022-08-21 18:24:53 +0300

    added links to navbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -4,6 +4,8 @@ import logoDark from "../../images/Giliop_3_b.png";
 import logoLight from "../../images/Giliop_3_w.png";
 import EmailIcon from "@mui/icons-material/Email";
 import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Topbar = ({ showMenu, setShowMenu }) => {
   const handleClick = () => {
@@ -27,6 +29,16 @@ const Topbar = ({ showMenu, setShowMenu }) => {
           <div className="itemContainer">
             <EmailIcon className="icon" />
             <span>giliopp@gmail.com</span>
+          </div>
+          <div className="itemContainer">
+            <a href="https://github.com/georgeiliop">
+              <GitHubIcon className="icon git" />
+            </a>
+          </div>
+          <div className="itemContainer">
+            <a href="https://www.linkedin.com/in/giorgos-iliopoulos-389704193/">
+              <LinkedInIcon className="icon" />
+            </a>
           </div>
         </div>
         <div className="right">
